30251c9fa785ddcb2f154345edd6ba56fcd6bbe0
SalmaYasserMoselhi <148722989+SalmaYasserMoselhi@users.noreply.github.com>
2026-05-24 16:02:59 +0300

    Delete day2 directory

--- day2/src/views/NotFoundView.vue
+++
@@ -1,9 +0,0 @@
-<script setup>
-import router from "@/router";
-import { onMounted } from "vue";
-
-onMounted(() => {
-  router.replace({ path: "/not-found" });
-});
-</script>
-<template>404</template>
